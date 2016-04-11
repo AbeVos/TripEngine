3a1de829c5f3eb29bacbbc3f2b147bb215afd1db
Abe Vos <abe_vos@msn.com>
2016-04-11 08:48:39 +0200

    Tweaked lightshaftshader

--- TripEngine/ObjectShowcase/Resources/Shaders/ppFragment.glsl
+++ TripEngine/ObjectShowcase/Resources/Shaders/ppFragment.glsl
@@ -2,7 +2,7 @@
 #version 450 core
 #define EXTINCTION 0.5
 #define NUM_SAMPLES 32
-#define INTERLEAVED_GRID_SIZE 8
+#define INTERLEAVED_GRID_SIZE 4
 
 layout(location = 0) out vec4 out_color;
 
@@ -48,22 +48,8 @@ void main()
 		currentPosition += stepSize * rayDirection.xyz;
 	}
 
-	result = clamp(result, 0, 1);
-
 	out_color = color * exp(- EXTINCTION) + vec4(result, 1) * exp(- EXTINCTION);
-	//out_color = vec4(rayOffset);
-
-	/*vec4 lightSpacePosition = lightMatrix * inverse(VPMatrix) * vec4(uv.x * 2 - 1, uv.y * 2 - 1, depth * 2 - 1, 1.0);
-
-	vec3 projCoords = lightSpacePosition.xyz / lightSpacePosition.w;
-	projCoords = 0.5 * (projCoords + 1);
-
-	float closestDepth = texture(shadowMap, 1 * (projCoords.xy)).r;
-	float currentDepth = projCoords.z - 0.001;
-
-	float attenuation = currentDepth > closestDepth ? 0.0 : 1.0;
-
-	out_color = vec4(attenuation);*/
+	//out_color = vec4(result, 1);
 
 	out_color = vec4(pow(out_color.rgb, vec3(exp(-1 / 2.2))), 1);
 }
